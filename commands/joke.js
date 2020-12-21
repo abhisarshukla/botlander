73d23e3374d67401a2a709ceb69db62cf3f56a96
abhisarshukla <abhisarshukla645@gmail.com>
2020-12-21 21:01:42 +0530

    Add fallback joke API to joke command

--- commands/joke.js
+++ commands/joke.js
@@ -14,7 +14,26 @@ module.exports = {
       .then((response) => {
         const jokeSetup = response.data.setup;
         const jokeDelivery = response.data.delivery;
-        message.channel.send(`**${jokeSetup}** \r *${jokeDelivery}*`);
+        if (jokeSetup == undefined || jokeDelivery == undefined) {
+          await axios({
+            method: "get",
+            url: "https://icanhazdadjoke.com/",
+            header: {
+              "Accept": "application/json",
+              "User-Agent": "Botlander Discord Bot"
+            }
+          })
+            .then((response) => {
+            const joke = response.data.joke;
+            message.channel.send(joke);
+          })
+            .catch((error) => {
+            console.error(error);
+            console.log("Above Error happened while doing some task.");
+          });
+        } else {
+          message.channel.send(`**${jokeSetup}** \r *${jokeDelivery}*`);
+        }
       })
       .catch((error) => {
         console.error(error);
