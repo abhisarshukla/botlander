d284bf9187c6dc5dea62129eca1ca1bd566d9be3
abhisarshukla <abhisarshukla645@gmail.com>
2020-12-21 21:12:02 +0530

    Refactor joke.js

--- commands/joke.js
+++ commands/joke.js
@@ -4,40 +4,27 @@ module.exports = {
   name: "joke",
   description: "A random Joke each time.",
   async execute(message, args) {
-    await axios({
+    const response = await axios({
       method: "get",
       url: "https://sv443.net/jokeapi/v2/joke/Any",
       params: {
         blacklistFlags: "racist,sexist",
       },
-    })
-      .then((response) => {
-        const jokeSetup = response.data.setup;
-        const jokeDelivery = response.data.delivery;
-        if (jokeSetup == undefined || jokeDelivery == undefined) {
-          await axios({
-            method: "get",
-            url: "https://icanhazdadjoke.com/",
-            header: {
-              "Accept": "application/json",
-              "User-Agent": "Botlander Discord Bot"
-            }
-          })
-            .then((response) => {
-            const joke = response.data.joke;
-            message.channel.send(joke);
-          })
-            .catch((error) => {
-            console.error(error);
-            console.log("Above Error happened while doing some task.");
-          });
-        } else {
-          message.channel.send(`**${jokeSetup}** \r *${jokeDelivery}*`);
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-        console.log("Above Error happened while doing some task.");
+    });
+    const jokeSetup = response.data.setup;
+    const jokeDelivery = response.data.delivery;
+    let joke = `**${jokeSetup}** \r *${jokeDelivery}*`;
+    if (jokeSetup == undefined || jokeDelivery == undefined) {
+      const response = await axios({
+        method: "get",
+        url: "https://icanhazdadjoke.com/",
+        header: {
+          Accept: "application/json",
+          "User-Agent": "Botlander Discord Bot",
+        },
       });
+      joke = response.data.joke;
+    }
+    message.channel.send(joke);
   },
 };
